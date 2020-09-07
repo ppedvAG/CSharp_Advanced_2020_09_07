dc8c4a78522887e79a4e2c10567adb1755be4d65
Kevin Winter <KevinW@ppedv.de>
2020-09-07 13:22:41 +0200

    .NET Vorstellung & Historie
    CSharp Features für 7, 7.1, 7.2, 7.3

---
+++ CSharp_Advanced/CSharp71_Features/Program.cs
@@ -0,0 +1,78 @@
+﻿using CSharp71_Features;
+using System;
+using System.Threading.Tasks;
+
+namespace CSharp71_Features
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Task.Delay(2000);
+
+            Console.WriteLine("Nach 2 Sekunden...wird das hier geschrieben");
+        }
+    }
+
+
+    public class MyClass
+    {
+        public void GetPublic() { }
+        private void GetPrivate() { }
+        internal void GetInternal() { }
+        protected void GetProtected() { }
+        protected internal void GetProtectedInternal() { }
+        protected private void GetPrivateProtected() { }
+    }
+
+
+    public class YourClass : MyClass
+    {
+        MyClass mc = new MyClass();
+        public void Show()
+        {
+            mc.GetPublic();
+            //mc.GetPrivate(); Not accessible as private members of a class are not accessible outside class.  
+            mc.GetInternal();
+            //mc.GetProtected(); Not accessible as protected members can be accessed only through inheritance and not by creating an object.  
+            mc.GetProtectedInternal();
+            //mc.GetPrivateProtected(); Not accessible as Private Protected members can be accessed only through inheritance in same assembly.                
+            GetPublic();
+            //GetPrivate(); Not accessible as private members of a class are not accessible outside class.  
+            GetInternal();
+            GetProtected();
+            GetProtectedInternal();
+            GetPrivateProtected();
+        }
+    }
+
+
+
+}
+
+
+
+//Outside the assembly  
+
+namespace ClassLibrary1
+{
+    public class YourClass1 : MyClass
+    {
+        MyClass mc = new MyClass();
+        public void show()
+        {
+            mc.GetPublic();
+            //mc.GetPrivate(); Not accessible as private members of a class are not accessible outside class.  
+            mc.GetInternal(); //Not accessible as internal members are not accessible outside it's assembly.  
+            //mc.GetProtected(); Not accessible as protected members can be accessed only through inheritance and not by creating an object.  
+            //mc.GetProtectedInternal(); Not accessible as Protected Internal members can not be accessed outside of assembly by creating an object.  
+            //mc.GetPrivateProtected(); Not accessible as Private Protected members are not accessible outside of assembly by creating an object or through inheritance.      
+            GetPublic();
+            //GetPrivate(); Not accessible as private members of a class are not accessible outside class.  
+            GetProtected();
+            //GetInternal(); Not accessible as internal members are not accessible outside it's assembly.  
+            GetProtectedInternal();
+            //GetPrivateProtected(); Not accessible as Private Protected members are not accessible outside of assembly by creating an object or through inheritance.    
+        }
+    }
+}
---
+++ CSharp_Advanced/CSharp7_Features/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+
+namespace CSharp7_Features
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Vorstellung - out - Variablen
+
+            int variableB = 5;
+            int result = Calculate(3, out variableB);
+            Console.WriteLine(variableB); // Variable soll 11 sein.
+            Console.WriteLine(result); // Die Variable von Result ist 33
+            #endregion
+
+            #region praktische Anwendung von out-Variablen mit int.TryParse
+
+            Console.Write("Bittte geben sie eine Zahl ein");
+            string inputZahl = Console.ReadLine();
+
+            // Bei der Eingabe : '10.' oder '.123.' wird es beim umwandeln mit Convert.ToInt32 einen Fehler geben
+            //int convertedNumber = Convert.ToInt32(inputZahl); // Kann Exception hervorrufen
+            //convertedNumber = int.Parse(inputZahl); // Kann Exception hervorrufen
+
+
+            int convertedNumber;
+
+            if (int.TryParse(inputZahl, out convertedNumber))
+            {
+                // Wenn ich in diesem Block mich befinde, ist die Zahl erfolgreich konventiert worden
+
+                Console.WriteLine(convertedNumber);
+            }
+
+            #endregion
+
+            #region Nullable Datatypes früheren C# Version
+            int zahl1 = -1; // -1 = neu angelegte Variable mit einem inital-wert
+
+            int? nullableIntegerVariable = null;
+
+
+            //Prüfe ob ein Wert zugewiesen wurde
+            if (!nullableIntegerVariable.HasValue)
+                nullableIntegerVariable = 23;
+
+
+            if (nullableIntegerVariable.HasValue)
+                Console.WriteLine(nullableIntegerVariable.Value);
+
+            #endregion
+
+
+            #region Verwenden von Tuple
+            Person person = new Person();
+            var result1 = person.GetCompleteName();
+            //result1.Item1 -> Vorname
+            //result1.Item2 -> Zweiten Vornamen
+            //result1.Item3 -> Nachname
+
+            //selbe schreibweise wie bei var result1 -> man verwendet item1, item2, item3
+            (string, string, string) result2 = person.GetCompleteName();
+
+            string vorname, nachname, zweiterVorname;
+
+            (vorname, zweiterVorname, nachname) = person.GetCompleteName();
+            //printf Nachahme in C#
+            Console.WriteLine("{0}, {1}, {2}", vorname, zweiterVorname, nachname);
+            // seit C# 7.0
+            Console.WriteLine($"Der Vorname ist: {vorname} - Zweiter Vorname: {zweiterVorname} - Nachname {nachname} ");
+
+            #endregion
+
+            #region Dekonstruktion von Typen
+            Kunde k = new Kunde { Id = 123, Name = "Andre", Stammkunde = true };
+            var (id, name, stammkunde) = k;
+            Console.WriteLine($"{id}{name}{stammkunde}");
+
+            #endregion
+
+            #region Literale in C#
+
+            int eineMio = 1000000;
+            int zweiMio = 1_000_000;
+
+            decimal myBankAccount = 12234343;
+            decimal myBankAccountWithLiteral = 123123123123m;
+
+            int hexValue = 0xFFF000;
+            byte binaer = 0b00001111;
+
+            Console.WriteLine($"{eineMio}{zweiMio}{myBankAccount}{myBankAccountWithLiteral}{hexValue}{binaer}");
+            #endregion
+
+            #region Rückgabe via Referenz
+            // Create an array of author names   
+            string[] authors = { "Mahesh Chand", "Mike Gold", "Dave McCarter", "Allen O'neill", "Raj Kumar" };
+
+            // Call a method that returns by ref    
+            ref string author4 = ref new Program().FindAuthor(3, authors);
+            Console.WriteLine("Original author:{0}", author4);
+
+            // Prints 4th author in array = Allen O'neill    
+            Console.WriteLine();
+
+            // Replace 4th author by new author. By Ref, it will update the array    
+            author4 = "Chris Sells";
+
+            // Print 4th author in array  
+            Console.WriteLine("Replaced author:{0}", authors[3]);
+
+            //Prints Chris Sells    
+            Console.ReadKey();
+
+
+            int[] zahlen = { 5, 7, 3, 433, 12, 23 };
+            ref int position = ref new Program().Zahlensuche(3, zahlen);
+            //position = 100_000_000;
+
+            //Console.WriteLine(zahlen[5]);
+            #endregion
+
+            Console.ReadLine();
+        }
+
+        public static int Calculate (int a,  out int b)
+        {
+            b = 11;
+
+            return a * b;
+        }
+
+        public ref string FindAuthor(int number, string[] names)
+        {
+
+            if (names.Length > 0)
+                return ref names[number]; // return the storage location, not the value    
+            throw new IndexOutOfRangeException($"{nameof(number)} not found.");
+
+        }
+
+        public ref int Zahlensuche(int gesuchteZahl, int[] zahlen)
+        {
+            for (int i = 0; i < zahlen.Length; i++)
+            {
+                if (zahlen[i] == gesuchteZahl)
+                    return ref zahlen[i];
+
+            }
+            throw new IndexOutOfRangeException();
+        }
+    }
+
+
+    public class Person
+    {
+        private string zweiterVornamen;
+        public string ZweiterVornamen
+        {
+            get => this.zweiterVornamen;
+            
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Variable muss befüllt sein");
+                this.zweiterVornamen = value;
+            }
+
+        }
+
+        //Auto-Property
+        public string Vorname { get; set; }
+
+        public string Nachname { get; set; }
+
+
+        //Deconstrutor Objekt wird auf seine Variablen aufgebröselt
+        public (string, string, string) GetCompleteName ()
+        {
+            return (Vorname, ZweiterVornamen, Nachname);
+        }
+    }
+
+    public class Kunde
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Stammkunde { get; set; }
+
+        public void Deconstruct (out int ID, out string Name, out bool Stammkunde)
+        {
+            ID = this.Id;
+            Name = this.Name;
+            Stammkunde = this.Stammkunde;
+        }
+    }
+}
